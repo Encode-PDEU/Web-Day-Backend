d7549144cb6fc62ef11cb1c934f33bbfa9202016
DhyanShah22 <dhyanshah229@gmail.com>
2024-07-30 08:57:06 +0530

    Deployment Commit

--- Backend/Models/questionModel.js
+++ Backend/Models/questionModel.js
@@ -4,6 +4,6 @@ const QuestionSchema = new mongoose.Schema({
   question: { type: String, required: true },
   options: { type: [String], required: true },
   answer: { type: String, required: true }
-});
+}, {timestamps: true});
 
 module.exports = mongoose.model('Question', QuestionSchema);
