998e33531323fa0f0bd68e082e8cb63ea0690088
MeetD99 <meetdholakia2074@gmail.com>
2024-08-23 01:42:00 +0530

    Final Changes for Intro Session

--- Backend/Controllers/questionController.js
+++ Backend/Controllers/questionController.js
@@ -3,33 +3,28 @@ const logger = require('../utils/logger');
 const promClient = require('prom-client');
 
 const counter = new promClient.Counter({
-  name: 'question_served',
+  name: 'questions_served',
   help: 'The number of questions served'
 });
 
-const getRandomQuestion = async (req, res) => {
+const getAllQuestions = async (req, res) => {
   try {
-    const count = await Question.countDocuments();
+    const questions = await Question.find();
+    console.log(questions);
+    const count = questions.length;
+
     console.log('Total questions count:', count);
     logger.info('Total questions count:', count);
+
     if (count === 0) {
       return res.status(404).json({ msg: 'No questions found' });
     }
-    const random = Math.floor(Math.random() * count);
-    const question = await Question.findOne().skip(random);
-    logger.info('Random question:', question);
-    console.log('Random question:', question);
 
-    if (!question) {
-      return res.status(404).json({ msg: 'No question found' });
-    }
+    counter.inc(count); // Increment the counter by the number of questions served
 
-    counter.inc();
-
-    res.json(question);
+    res.json(questions);
   } catch (err) {
-    logger.error('Error in /random-question endpoint:', err.message);
-    logger.error('Error in /random-question endpoint:', err.message);
+    logger.error('Error in /all-questions endpoint:', err.message);
     res.status(500).send('Server Error');
   }
 };
@@ -49,4 +44,4 @@ const checkAnswer = async (req, res) => {
   }
 };
 
-module.exports = { getRandomQuestion, checkAnswer };
+module.exports = { getAllQuestions, checkAnswer };
