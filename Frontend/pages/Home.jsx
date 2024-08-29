3e333ce2444f3e5784482db86b156017e9c93b91
MeetD99 <meetdholakia2074@gmail.com>
2024-08-29 23:19:50 +0530

    removed browser button links

--- Frontend/pages/Home.jsx
+++ Frontend/pages/Home.jsx
@@ -123,7 +123,7 @@ const Home = () => {
                 <div className="navbar">
                     <div className="left">
                         <div className="logo">
-                            <a href="https://www.instagram.com/encode_pdeu?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target='_blank'><img src={Logo} alt="" /></a>
+                            <a><img src={Logo} alt="" /></a>
                         </div>
                     </div>
                     <div className="center">
@@ -131,8 +131,8 @@ const Home = () => {
                     </div>
                     <div className="browser-buttons">
                         <span className="button minimize"></span>
-                        <a href="https://chat.whatsapp.com/CbZ618g7HCOCMOgRoV4W8m" target='_blank'><span className="button maximize"></span></a>
-                        <a href="https://www.linkedin.com/company/encode-pdpu/mycompany/" target='_blank'><span className="button close"></span></a>
+                        <a><span className="button maximize"></span></a>
+                        <a><span className="button close"></span></a>
                     </div>
                 </div>
                 <div className="main">
